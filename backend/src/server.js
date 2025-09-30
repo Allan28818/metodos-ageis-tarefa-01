3b0fe814d990d9465c9ad2eb246163906fb526e8
Allanzin178 <allan.miron@hotmail.com>
2025-09-30 17:45:31 -0300

    Tarefa feita, login feito

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,15 @@
 import express from 'express'
 
+// Rotas
+import userRoutes from './routes/user.routes.js'
+import authRoutes from './routes/auth.routes.js'
+
 const app = express()
 const port = 3000
+
+app.use(express.json())
+app.use('/', userRoutes)
+app.use('', authRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
